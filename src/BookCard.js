1176ff357a3f1a5fe9be028ebde410bdfc816a9c
Emad <elbembo@users.noreply.github.com>
2021-07-20 05:57:34 +0200

    http to https

--- src/BookCard.js
+++ src/BookCard.js
@@ -18,7 +18,15 @@ export class BookCard extends Component {
             <div className="book">
                 <div className="book-top">
                     <Link to={`/info/${this.props.book.id}`} >
-                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.book.imageLinks.thumbnail.replace(/^http:\/\//i, 'https://')})` }}></div>
+                        <div className="book-cover" style={
+                            /*
+                            * use replace function to avoid mixed content warnning from the browser 
+                            */
+                            {
+                                width: 128,
+                                height: 192,
+                                backgroundImage: `url(${this.props.book.imageLinks.thumbnail.replace(/^http:\/\//i, 'https://')})`
+                            }}></div>
                         <div className="book-cover-overlay" ><span>More&nbsp;info</span></div>
                     </Link>
                     <ShelfBotton onChange={this.props.onChange} book={this.props.book} />
@@ -33,7 +41,7 @@ export class BookCard extends Component {
                 </div>
                 <a className='read-now' target='_blank' rel="noopener noreferrer" href={`https://www.google.com.eg/books/edition/_/${this.props.book.id}?hl=en&gbpv=1`}>
                     Read now
-                    </a>
+                </a>
             </div>
 
 
