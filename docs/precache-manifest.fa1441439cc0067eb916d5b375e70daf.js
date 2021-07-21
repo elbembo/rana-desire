c8d06a60a0b7bf37ebaa7d6eaa945ed0f4edf61a
Emad <elbembo@users.noreply.github.com>
2021-07-21 05:23:42 +0200

    docs

---
+++ docs/precache-manifest.fa1441439cc0067eb916d5b375e70daf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "99d958a437812fb42c29",
+    "url": "/static/css/main.fa4232d7.chunk.css"
+  },
+  {
+    "revision": "99d958a437812fb42c29",
+    "url": "/static/js/main.99d958a4.chunk.js"
+  },
+  {
+    "revision": "a54b44aab614e2abf184",
+    "url": "/static/js/1.a54b44aa.chunk.js"
+  },
+  {
+    "revision": "baf6f7d8b5658cb62517",
+    "url": "/static/js/2.baf6f7d8.chunk.js"
+  },
+  {
+    "revision": "1afd3bf083d7c67e9e46",
+    "url": "/static/js/runtime~main.1afd3bf0.js"
+  },
+  {
+    "revision": "03b486941bc9244acf226ebca1527fba",
+    "url": "/static/media/gold.03b48694.png"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "e658a2dd8ba94f2e886ff936a48fed1a",
+    "url": "/index.html"
+  }
+];
